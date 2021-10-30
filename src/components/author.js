9678ff7f5b9dfe4d2720f58b1e3771acbc1d1969
inksharedsolutions <webdev.inkshared@gmail.com>
2021-10-30 08:23:18 +0800

    adjust padding

--- src/components/author.js
+++ src/components/author.js
@@ -5,9 +5,10 @@ const Author = () =>{
     return(
         <>
             <section className="upper-main" id="main-upper-author">
+                <div className="container-gt">
                 <div className="grid-two-columns">
 
-                     <div className="grid-child" id="author-contents">
+                    <div className="grid-child" id="author-contents">
 
                         <p>
                         The author has a PhD in Mechanical Engineering from the University of Michigan, Ann Arbor. He had been the Professor of Mechanical Engineering at Iowa State University in the Mechanical Engineering Department for more than forty years. 
@@ -17,16 +18,18 @@ const Author = () =>{
                             <span>&nbsp;Shyam &nbsp;</span>
                             <span>Bahadur</span>
                         </h1>
-                        
+
                         <span className="author-tagline">Author & Writer</span>
 
-                     </div>
+                        </div>
 
-                    <div className="grid-child" id="author-image-container">
+                        <div className="grid-child" id="author-image-container">
                         <img alt="author-img" src={AuthorImg}/>
-                    </div>
+                        </div>
 
+                    </div>
                 </div>
+                
             </section>
         </>
     )
